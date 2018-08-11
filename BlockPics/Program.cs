0cce643c8abbd5ec216001572ffc8344dabd3259
Kieran <kieran@harkin.me>
2018-08-11 13:52:02 +0800

    write to files only

--- BlockPics/Program.cs
+++ BlockPics/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
@@ -35,15 +36,12 @@ namespace BlockPics
             zqt.Start();
 
             var igen = new ProcessStartInfo("ffmpeg");
-            igen.RedirectStandardInput = true;
-
-            var ibuf = new byte[4 * 1000 * 1000];
-
             while (IsRunning)
             {
                 var block = await BlockStream.ReceiveAsync();
                 var block_hash = block.Hash.ToString();
                 var block_data = block.ToArray();
+
                 var pixels = block_data.Length / 3;
 
                 var sres = (int)Math.Ceiling(Math.Sqrt(pixels));
@@ -51,17 +49,19 @@ namespace BlockPics
 
                 Console.WriteLine($"Adding {padding} bytes padding..");
 
-                igen.Arguments = $"-y -f rawvideo -pix_fmt rgb24 -s {sres}x{sres} -i - -vframes 1 {block_hash}.png";
+                await File.WriteAllBytesAsync("block.dat", block_data.Concat(new byte[padding]));
+
+                igen.Arguments = $"-y -f rawvideo -pix_fmt rgb24 -s {sres}x{sres} -i block.dat -vframes 1 {block_hash}.png";
 
                 var fp = Process.Start(igen);
-                await fp.StandardInput.BaseStream.WriteAsync(block_data.Concat(new byte[padding]));
+                //await fp.StandardInput.BaseStream.WriteAsync(block_data.Concat(new byte[padding]));
 
                 fp.WaitForExit();
 
                 try
                 {
                     var media = await authClient.UploadMedia($"{block_hash}.png");
-                    await authClient.PostNewStatus($"#Bitcoin tip updated! {block_hash}", null, new List<int>() { media.Id });
+                    await authClient.PostNewStatus($"#Bitcoin tip updated! {block_hash} ({block.TxnCount.Value.ToString("#,###")} txns, moving {block.Txns.Sum(a => a.TxOut.Sum(b => b.Value / 1e-8)).ToString("#,##0.0000 BTC")})", null, new List<int>() { media.Id });
                     
                     Console.WriteLine($"Block posted! {media.Url}");
                 }
